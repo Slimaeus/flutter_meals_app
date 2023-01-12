42f68f76ebe06b10abe2d310017af70f4c48ed38
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-12 20:36:07 +0700

    Add category meals route

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs["id"] as String;
+    final categoryTitle = routeArgs["title"] as String;
+    return Scaffold(
+      appBar: AppBar(title: Text(categoryTitle)),
+      body: Center(
+        child: Text('The Recipes For The Category!'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meals_app/categories_screen.dart';
+import 'categories_screen.dart';
+import 'category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,22 +8,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-          primarySwatch: Colors.pink,
-          colorScheme:
-              ThemeData().colorScheme.copyWith(secondary: Colors.amber),
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              headline6: TextStyle(
-                  fontSize: 24,
-                  fontFamily: 'RobotoCondensed',
-                  fontWeight: FontWeight.bold))),
-      home: CategoriesScreen(),
-    );
+        title: 'DeliMeals',
+        theme: ThemeData(
+            primarySwatch: Colors.pink,
+            colorScheme:
+                ThemeData().colorScheme.copyWith(secondary: Colors.amber),
+            canvasColor: Color.fromRGBO(255, 254, 229, 1),
+            fontFamily: 'Raleway',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                headline6: TextStyle(
+                    fontSize: 24,
+                    fontFamily: 'RobotoCondensed',
+                    fontWeight: FontWeight.bold))),
+        home: CategoriesScreen(),
+        routes: {'/category-meals': (context) => CategoryMealsScreen()});
   }
 }
 
