49cc4fc418d795d34feafc438878aa76d22fc859
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-13 08:30:03 +0700

    Add widgets, screens folders and change some paths

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'categories_screen.dart';
-import 'category_meals_screen.dart';
+import 'package:flutter_meals_app/screens/categories_screen.dart';
+import 'package:flutter_meals_app/screens/category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
